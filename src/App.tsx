754816b543572ca91bf0d614446ca29b8e6b7587
ndzy01 <46743100+ndzy01@users.noreply.github.com>
2024-03-03 19:08:36 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => {
     d3: '',
     d4: '',
     links: [
-      { src: 'https://ndzy01.gitee.io/ndzy-docs/', name: '文档', color: '' },
+      { src: 'https://www.ndzy01.com/ndzy-docs', name: '文档', color: '' },
       { src: 'https://www.yuque.com/u22409297/fqv2ol', name: 'blog' },
       { src: 'https://www.yuque.com/u22409297/aqgf01', name: '数据结构与算法' },
       { src: 'https://labuladong.github.io/algo/di-ling-zh-bfe1b/xue-xi-sua-01220/', name: '算法小抄' },
