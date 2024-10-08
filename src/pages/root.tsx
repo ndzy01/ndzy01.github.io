8891eddcaf690ed7d4010cfe5f5aac1649c7de5a
ndzywork998 <zhang.yi@pitayagames.com>
2024-10-08 18:32:32 +0800

    优化

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,5 +1,5 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons"
-import { FloatButton, MenuProps } from "antd"
+import { Button, MenuProps } from "antd"
 import { Menu } from "antd"
 
 import React, { useState } from "react"
@@ -34,47 +34,54 @@ const Root = () => {
 
   return (
     <div className="container">
-      <aside
-        className="sidebar"
-        style={{ display: collapsed ? "none" : "block" }}
-      >
-        <header className="header">
-          <h1>欢迎 {data?.name || ""}</h1>
-          <div className="user-info">
-            <a
-              onClick={() => {
-                sessionStorage.setItem("token", "")
-                sessionStorage.setItem("name", "")
-                navigate("/login")
-                window.location.reload()
-              }}
-            >
-              注销
-            </a>
-          </div>
-        </header>
-        <Menu
-          style={{ borderRadius: 5 }}
-          onClick={(e) => {
-            navigate(e.key)
-          }}
-          defaultSelectedKeys={["/"]}
-          mode="inline"
-          items={items}
+      {!collapsed ? (
+        <aside
+          className="sidebar"
+          style={{ display: collapsed ? "none" : "block" }}
+        >
+          <header className="header">
+            <h1>欢迎 {data?.name || ""}</h1>
+            <div className="user-info">
+              <a
+                onClick={() => {
+                  sessionStorage.setItem("token", "")
+                  sessionStorage.setItem("name", "")
+                  navigate("/login")
+                  window.location.reload()
+                }}
+              >
+                注销
+              </a>
+            </div>
+          </header>
+          <Menu
+            style={{ borderRadius: 5 }}
+            onClick={(e) => {
+              navigate(e.key)
+            }}
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+          <Button
+            style={{ marginTop: 16 }}
+            icon={<MenuFoldOutlined />}
+            onClick={() => setCollapsed(true)}
+          />
+        </aside>
+      ) : (
+        <Button
+          style={{ margin: 8 }}
+          icon={<MenuUnfoldOutlined />}
+          onClick={() => setCollapsed(false)}
         />
-      </aside>
+      )}
+
       <main className="main-content">
         <section className="content" id="ndzy-content">
           <Outlet />
         </section>
       </main>
-
-      <FloatButton.Group shape="circle">
-        <FloatButton
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          onClick={() => setCollapsed(!collapsed)}
-        />
-      </FloatButton.Group>
     </div>
   )
 }
