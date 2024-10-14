d109923f13bdca96de092651c5358a3ab26359e4
ndzywork998 <zhang.yi@pitayagames.com>
2024-10-14 18:33:31 +0800

    优化

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,7 +5,7 @@ const defaultMeta = {
   charset: {
     charset: "UTF-8",
   },
-  viewport: "width=device-width, initial-scale=1.0,user-scalable=no",
+  viewport: "width=device-width,initial-scale=1.0,user-scalable=no",
 }
 
 export default defineConfig({
